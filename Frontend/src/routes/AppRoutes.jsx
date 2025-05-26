18ac838f96832594135920e196c02f144bc8c027
Shaik Afiya <shaikafiya9676@gmail.com>
2025-05-26 20:58:15 +0530

    okay done

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -12,8 +12,8 @@ const AppRoutes = () => {
 
             <Routes>
                 {/* <Route path="/" element={<UserAuth><Home /></UserAuth>} /> */}
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Login/>} />
+                <Route path="/login" element={<Home/>} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/project" element={<UserAuth><Project /></UserAuth>} />
                 {/* <Route path="/project" element={<Project />} /> */}
